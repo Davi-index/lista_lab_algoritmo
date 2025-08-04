c58d5204dec3ebbd52a69dcfb0fc85ab2d0ad370
Davi Alves Maia <maiadavi3266@gmail.com>
2025-08-04 08:10:58 -0300

    debug

--- Algoritmos/lista 4.c
+++ Algoritmos/lista 4.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    float nota1, nota2, media;
-
-    printf("Digite a primeira nota: ");
-    scanf("%f", &nota1);
-
-    printf("Digite a segunda nota: ");
-    scanf("%f", &nota2);
-
-    media = (nota1 + nota2) / 2.0;
-
-    printf("Media aritmetica: %.2f\n", media);
-
-    if (media >= 6.0) {
-        printf("Aprovado\n");
+void calcularparidade(int num) {
+    if(num % 2 == 0){
+        printf("o numero %d e par\n", num);
     } else {
-        printf("Reprovado\n");
+        printf("o Numero %d e impar\n", num);
     }
-
-    return 0;
 }
 
-//Algoritmos para treinar estrutura de decisão
+int main() {
+    int num;
+printf("Digite um numero: ");
+scanf("%d", &num);
+    calcularparidade(num);
+    return 0;
+}
