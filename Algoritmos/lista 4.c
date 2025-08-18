a0cd973bd21a0879c9b18b8f8a28bc656b9dc844
Davi Alves Maia <maiadavi3266@gmail.com>
2025-08-18 13:07:42 -0300

    atualização de exercicio 4

--- Algoritmos/lista 4.c
+++ Algoritmos/lista 4.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-void calcularparidade(int num) {
-    if(num % 2 == 0){
-        printf("o numero %d e par\n", num);
-    } else {
-        printf("o Numero %d e impar\n", num);
+void maiormenor(int num1, int num2) {
+    if(num1 > num2){
+        printf("o numero %d e maior\n", num1);
+    } else if(num2 > num1){
+        printf("o Numero %d e maior\n", num2);
     }
 }
 
 int main() {
-    int num;
-printf("Digite um numero: ");
-scanf("%d", &num);
-    calcularparidade(num);
+    int num1, num2;
+printf("Digite dois numeros: ");
+scanf("%d %d", &num1, &num2);
+    maiormenor(num1, num2);
     return 0;
 }
