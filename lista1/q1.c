639b9510a779b71a44abf79834abec286db66d91
Davi Alves Maia <72363511+Davi-index@users.noreply.github.com>
2025-09-25 21:26:16 -0300

    Adição de lista
    Adição de lista de exercicios de Lab Estrutura de Dados I

---
+++ lista1/q1.c
@@ -0,0 +1,5 @@
+/*
+  Um ponteiro em C é uma variável que armazena o endereço de memória de outra variável.
+  Ele serve para acessar e manipular diretamente o conteúdo da memória,
+  a criação de estruturas dinâmicas (como listas e árvores) e o gerenciamento manual de memória.
+ */
