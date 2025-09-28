5510b4a9f0de4e2a989660cd93b9f1668d73cdd6
Davi Alves Maia <72363511+Davi-index@users.noreply.github.com>
2025-09-28 00:01:54 -0300

    upload questão 4

---
+++ lista1/q4.c
@@ -0,0 +1,9 @@
+/*
+Na expressão float *ptr;, o tipo float se refere ao tipo dos dados apontados pelo ponteiro.
+
+ptr é uma variável do tipo "ponteiro para float".
+O operador * indica que ptr é um ponteiro.
+O tipo float indica que o ponteiro irá apontar para um valor do tipo float.
+Ou seja: O que é do tipo float?
+O valor no qual ptr aponta (ou seja, o dado referenciado por *ptr) é do tipo float.
+*/
