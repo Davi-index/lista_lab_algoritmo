9171022e74389b7cfd95ad2c37b87f154d8cb948
Davi Alves Maia <maiadavi3266@gmail.com>
2025-10-01 13:26:47 -0300

    Create q8.c
    resposta da questão 8 da primeira lista de Estrutura da dados 1

---
+++ lista1/q8.c
@@ -0,0 +1,6 @@
+/*
+Resposta:
+Se o ponteiro px aponta para a variável x, podemos acessar e modificar o valor de x usando o ponteiro.
+Para dividir x por 3 sem usar diretamente x, usamos a expressão: *px = *px / 3;
+Logo baixo está um exemplo em código:
+*/
