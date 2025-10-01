7e0a9272322c8291f45aa491fe31c91c27cbfcfa
Davi Alves Maia <maiadavi3266@gmail.com>
2025-10-01 13:27:28 -0300

    update q8.c
    criação da exemplo de código para a resposta da questão 8

--- lista1/q8.c
+++ lista1/q8.c
@@ -4,3 +4,17 @@ Se o ponteiro px aponta para a variável x, podemos acessar e modificar o valor 
 Para dividir x por 3 sem usar diretamente x, usamos a expressão: *px = *px / 3;
 Logo baixo está um exemplo em código:
 */
+
+#include <stdio.h>
+
+int main() {
+    int x = 12;
+    int *px = &x;
+
+    // Divide x por 3 usando apenas o ponteiro px
+    *px = *px / 3;
+
+    printf("Novo valor de x: %d\n", x); // Saída: Novo valor de x: 4
+
+    return 0;
+}
