d1657c159f871029037f90a13219f262a3562503
Davi Alves Maia <maiadavi3266@gmail.com>
2025-10-02 23:40:10 -0300

    resolução da questão 9

---
+++ lista1/q9.c
@@ -0,0 +1,11 @@
+// A afirmativa V é falsa porque pti armazena um endereço, não o valor 10.
+// código abaixo para exemplificar a questão
+
+#include <stdio.h>
+
+
+int main()
+{
+    /* códigozim */
+    return 0;
+}
