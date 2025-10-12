ddf4051754674e4b464de98f75241c7b00fa7182
Davi Alves Maia <72363511+Davi-index@users.noreply.github.com>
2025-10-12 20:48:30 -0300

    Update q3.c
    atualização e finalização da questão 3

--- lista2/q3.c
+++ lista2/q3.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 int main()
 {
     int *ptr; // ponteiro recém-criado, não inicializado
-
     printf("Endereço armazenado em ptr: %p\n", (void *)ptr);
     printf("Conteúdo apontado por ptr: %d\n", *ptr); // comportamento indefinido
 
