e476af733891f00c1b01e0c29c61b19d13976fb7
Davi Alves Maia <maiadavi3266@gmail.com>
2025-11-15 17:48:40 -0300

    Update ordenacao.py
    Criação recursiva do algoritmo

--- Algoritmos/ordenacao.py
+++ Algoritmos/ordenacao.py
@@ -1,1 +1,12 @@
 # código baseado e com intuíto de aplicar e explicar os algoritmos de ordenação
+
+# "Quick Sort":
+def quicksort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)-1
+    if inicio < fim:
+        p = partition(lista, inicio, fim)
+        # recursivamente na sublista à esquerda (menores)
+        quicksort(lista, inicio, p-1)
+        # recursivamente na sublista à direita (maiores)
+        quicksort(lista, p+1, fim)
