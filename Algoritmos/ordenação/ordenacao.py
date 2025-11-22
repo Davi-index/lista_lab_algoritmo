38cdb5186e6ee6cde3e2e622af62d52202c6aa21
Davi Alves Maia <maiadavi3266@gmail.com>
2025-11-21 23:49:32 -0300

    Update "Insertion Sort"
     Aplicação do algoritmo de ordenação "insertion sort" provavelmente  o algoritmo de ordenação mais curto para se aplicar em código

--- Algoritmos/ordenação/ordenacao.py
+++ Algoritmos/ordenação/ordenacao.py
@@ -66,3 +66,12 @@ def merge(lista, inicio, meio, fim):
             top_right = top_right + 1
 
 # "Insertion Sort":
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista [j] > chave:
+            lista[j+1] = lista [j]
+            j = j - 1
+        lista[j+1] = chave
